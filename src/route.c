92e179f2515b22b45550fd736316ac02741bea8b
Tom Dohrmann <erbse.13@gmx.de>
2026-08-02 12:37:41 +0200

    keep acquisition timestamp when route isn't updated
    RFC 4861 specifies that router advertisements should be sent no more
    than once every MIN_DELAY_BETWEEN_RAS (3 seconds). In practice though,
    there are several ISPs (Comcast, Spectrum, Vodafone) that send out
    router advertisements at higher rates.
    
    Previously this caused dhcpcd to never update the route until it
    eventually expired and was removed by the kernel.
    
    This happened because the rt_acquired timestamp was out of sync with
    the kernel expiry tracking. rt_acquired was updated every time a router
    advertisement was processed **even when the route wasn't updated in the
    kernel**. When router advertisements were sent out frequently,
    rt_acquired was updated frequently, the difference between the old and
    new rt_acquired values never became great enough for rt_cmp_lifetime to
    return 1 and the kernel's routing table was never updated.
    
    To fix this, keep the old rt_acquired value iff the kernel's routing
    table wasn't updated. This ensures that rt_acquired stays in sync with
    the expiry value in the kernel's routing table.

--- src/route.c
+++ src/route.c
@@ -743,7 +743,12 @@ rt_doroute(rb_tree_t *kroutes, struct rt *rt)
 		{
 			if (!rt_add(kroutes, rt, or))
 				return false;
+		} else {
+#ifdef HAVE_ROUTE_LIFETIME
+			rt->rt_acquired = or->rt_acquired;
+#endif
 		}
+
 		rb_tree_remove_node(&ctx->routes, or);
 		rt_free(or);
 	} else {
